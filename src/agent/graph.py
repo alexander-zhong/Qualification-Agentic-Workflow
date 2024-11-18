d80fbecd8266e09ba6d9e0369074b506d2420055
alexander-zhong <alexanderzousky@gmail.com>
2024-11-18 15:23:10 -0800

    updated the first half of the supervisor agent

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -3,57 +3,147 @@ Structure of the category/grader agent - grading company on the rubric of each c
 """
 
 
-
+import json
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.graph import MessagesState 
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import SystemMessage, AIMessage
 from langchain_core.tools import tool
+from src.agent.state import State
 
 
 
 
 # Tool
 @tool
-def query(message: str):
+def query_list_company(message: str):
     """Querys the vector db. The arguments accepted in message str is ONLY company or search_company
-    
+
     Args:
-        message: must be "company" or "search_company" without quotations
+        message: keyword on what the event is about 
     """
-    if message == "company":
-        return "Microsoft"
-    else:
-        return "Microsoft is a very AI focused company with lots of policies around AI safety"
+    
+    return llm.invoke("Give a random list of 10 tech comapanies and return strictly only JSON with the list of just names. No other text")
+
+
 
 llm = ChatOpenAI(model="gpt-4o")
-rag_llm = llm.bind_tools([query])
+supervisor_agent = llm.bind_tools([query_list_company])
 
-sys_msg = SystemMessage(content="You are a search agent that queries for information and give a score on how much you like the company.")
 
-def rag_llm_node(state: MessagesState):
-    return {"messages": [rag_llm.invoke([sys_msg] + state["messages"])]}
+def query_list_node(state: State):
+    result = llm.invoke("Give a random list of 10 tech comapanies and return strictly only list of just names seperated by commas. No other text")
+    result = result.content 
+    candidates = [item.strip() for item in result.split(",")]
+    return {**state, "candidate_list": candidates}
+
+
+def supervisor_agent_node(state: State):
+    
+    # recieves the candidates list
+    candidates = ", ".join(state["candidate_list"])
+    
+    prompt = """"You part of a grading assistant. You're are solely responsible for identifying if there is any candidates left to grade. If there is, you output json.
+                Follow this structured process to reason through the next step:
+                """
+
+    
+    if (len(state["candidate_list"]) == 0):
+        prompt_empty = """
+                There is currently no companies in your state. You are given these two options: Please output one of the two outputs 
+                 
+                 1. Prompt the tool of querying a new list companies. Output query_list_company without quotation marks and additional text.
+                 2. After one iteration of query_list_company call, go to end node. Output END without quotation marks and additional text.
+                 
+                You MUST only output the given outputs Literal[query_list_company, END].
+        """
+
+        prompt = prompt + prompt_empty
+    else:
+        candidate = state["candidate_list"][0]
+        
+        prompt_non_empty = f"""
+                    There are candidates in the list. The candidate is {candidate}. 
+                    1. Identify whelther this candidate is a "company" or a "speaker" then output in this in JSON format. Below are examples you must follow. Output only JSON without any additional text.
+                    candidate_type must be either "company" or "speaker" and candidate_name must be {candidate}
+                    
+                    Example:
+                    {{ "candidate_name": "Microsoft", "candidate_type": "company"}}
+                    Example 2:
+                    {{"candidate_name": "John Doe", "candidate_type": "speaker"}}
+                    
+                    """
+        prompt = prompt + prompt_non_empty
+        
+    
+    sys_msg = SystemMessage(content=prompt)
+    output = llm.invoke([sys_msg] + state["messages"])
+    print(output)
+    print("\n")
+    return {**state, "messages": state["messages"] + [AIMessage(content=output.content)]}
+
+
+def planner_agent_node(state: State):
+    data = {}
+    try: 
+        data = json.loads(state["messages"][-1].content)
+    except json.JSONDecodeError as e:
+        pass # TODO
+    
+    return {**state, "candidate_type": data["candidate_type"], "current_candidate": state["candidate_list"][0], "candidate_list": state["candidate_list"][1:]}
+
+def supervisor_agent_condition(state: State):
+    output = state["messages"][-1].content
+    if (output == "query_list_company"):
+        return "query_list_node"
+    elif (output == "END"):
+        return END
+    else:
+        try: 
+            data = json.loads(output)
+        except json.JSONDecodeError as e:
+            return END
+        return "planner_agent_node" # TODO this is supposed to be the next node
+
 
 # tools that the agent has access to
-tools = [query]
+tools = [query_list_company]
 
 # Define the agent workflow graph
-workflow = StateGraph(MessagesState)
+workflow = StateGraph(State)
 
 # Add the node to the graph
-workflow.add_node("llm", rag_llm_node)
-workflow.add_node("tools", ToolNode(tools))
+workflow.add_node("supervisor_agent_node", supervisor_agent_node)
+workflow.add_node("query_list_node", query_list_node)
+workflow.add_node("planner_agent_node", planner_agent_node)
 
 # Defining the architecture 
-workflow.add_edge(START, "llm")
+workflow.add_edge(START, "supervisor_agent_node")
 workflow.add_conditional_edges(
-    "llm",
-    tools_condition,
+    "supervisor_agent_node",
+    supervisor_agent_condition,
+    {END: END, "query_list_node": "query_list_node", "planner_agent_node": "planner_agent_node"}
 )
-workflow.add_edge("tools", "llm")
-
+workflow.add_edge("query_list_node", "supervisor_agent_node")
+workflow.add_edge("planner_agent_node", END)
 
 # Compile the workflow into an executable graph
 graph = workflow.compile()
-graph.invoke({"messages": {"role": "user", "content": "Can you look at company for me and then search for company?"}})
+# graph.invoke({
+#         "messages": [],
+#         "candidate_type": None,  
+#         "current_candidate": None,  
+#         "company_rubric": {
+#             "company_mission_alignment": 1,
+#             "relevant_events": 1,
+#             "sponsorship_history": 1,
+#             "geographic_relevance": 1,
+#         },
+#         "speaker_rubric": {
+#             "speaker_expertise_alignment": 1,
+#             "company_relevance": 1,
+#             "geographic_relevance": 1,
+#         },
+#         "candidate_list": [],  
+#     })
--- src/agent/state.py
+++ src/agent/state.py
@@ -2,13 +2,37 @@
 
 
 from typing_extensions import TypedDict
-from typing import Annotated
+from typing import Annotated, Optional, Literal
 from langgraph.graph import MessagesState # prebuilt msg state when needed
 from langchain_core.messages import AnyMessage
 from langgraph.graph.message import add_messages
+from pydantic import BaseModel, field_validator, ValidationError
+
 
 
 # Keeps track of the message states - we can add more keys here for rubric data and etc.
+
+
+class SpeakerRubric(TypedDict):
+    speaker_expertise_alignment: Literal[1, 2, 3, 4, 5]
+    company_relevance: Literal[1, 2, 3, 4, 5]
+    geographic_relevance: Literal[1, 2, 3, 4, 5]
+    
+class CompanyRubric(TypedDict):
+    company_mission_alignment: Literal[1, 2, 3, 4, 5]
+    relevant_events: Literal[1, 2, 3, 4, 5]
+    sponsorship_history: Literal[1, 2, 3, 4, 5]
+    geographic_relevance: Literal[1, 2, 3, 4, 5]
+
+
 class State(TypedDict):
-    messages: Annotated[list[AnyMessage], add_messages] 
+    messages: Annotated[list[AnyMessage], add_messages]
+    candidate_type: Optional[Literal["speaker", "company"]]
+    current_candidate: Optional[str]
+    company_rubric: CompanyRubric
+    speaker_rubric: SpeakerRubric
+    candidate_list: list[Optional[str]]
+    
+    
+    
 
